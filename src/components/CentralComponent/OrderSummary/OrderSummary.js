7dcbef924d4a43af7eb2a6e082a94cd4e1c6a5d3
Miguel Frazão <jmpfrazao@gmail.com>
2019-01-29 12:48:31 +0000

    ShouldComponentUpdate added to Modal

--- src/components/CentralComponent/OrderSummary/OrderSummary.js
+++ src/components/CentralComponent/OrderSummary/OrderSummary.js
@@ -1,31 +1,33 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
-const orderSummary = (props) => {
 
-  const itemsSummary = Object.keys(props.items)
-    .map(itemKey => {
-      return (
-        <li key={itemKey}>
-          <span style={{ textTransform: 'capitalize' }}>{itemKey}</span>: {props.items[itemKey]}
-        </li>
-      )
-    })
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>Selected Items</p>
-      <ul>
-        {itemsSummary}
-      </ul>
-      <p><strong>Total Price: {props.factor.toFixed(2)}</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.modalCancelled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.modalContinued}>CONTINUE</Button>
-    </Aux>
-  )
+class OrderSummary extends Component {
 
+  render() {
+    const itemsSummary = Object.keys(this.props.items)
+      .map(itemKey => {
+        return (
+          <li key={itemKey}>
+            <span style={{ textTransform: 'capitalize' }}>{itemKey}</span>: {this.props.items[itemKey]}
+          </li>
+        )
+      })
+
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>Selected Items</p>
+        <ul>
+          {itemsSummary}
+        </ul>
+        <p><strong>Total Price: {this.props.factor.toFixed(2)}</strong></p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.modalCancelled}>CANCEL</Button>
+        <Button btnType="Success" clicked={this.props.modalContinued}>CONTINUE</Button>
+      </Aux>
+    );
+  }
 }
 
-export default orderSummary;
+export default OrderSummary;
